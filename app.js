05aa474212c15685263b834d10e9db6b0cdfe49b
Adetolar <tolaprince2003@gmail.com>
2022-09-22 15:44:36 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const counter = document.getElementById('num');
+
+let item = document.getElementsByClassName('btn')
+
+let score = 0
+
+btn[0].addEventListener('click',()=>{
+        score--
+        zero.textContect=score
+});
+
+btn[1].addEventListener('click',()=>{
+    score = 0
+    zero.textContect=score
+});
+
+btn[2].addEventListener('click',()=>{
+    score++
+    zero.textContect=score
+});
